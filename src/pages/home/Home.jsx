83e14bdf8f7c3ab2e547a19f6494582fcfcf3afb
Furtive Pygmy <hernanhael@gmail.com>
2025-04-28 23:11:07 -0300

    feat: starting routing app

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-import MainLayout from '../../layouts/main/MainLayout'
 import CardEvent from '../../components/card-event/CardEvent'
 import './home.scss'
 
@@ -7,14 +6,12 @@ export default function Home({ events }) {
 
   return (
     <div className="home-container">
-      <MainLayout>
-        <section className="event-section">
-          <h2>Events</h2>
-          {events.events.map((event) => (
-            <CardEvent event={event} key={event._id} />
-          ))}
-        </section>
-      </MainLayout>
+      <section className="event-section">
+        <h2>Events</h2>
+        {events.events.map((event) => (
+          <CardEvent event={event} key={event._id} />
+        ))}
+      </section>
     </div>
   )
 }
---
+++ src/pages/upcoming-events/UpcomingEvents.jsx
@@ -0,0 +1,5 @@
+import './upcoming-events.scss'
+
+export default function UpcomingEvents() {
+  return <div>UpcomingEvents</div>
+}
